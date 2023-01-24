1c89e422a4ca4e7ace6e54957047bbdbba71ab92
Andrew191103 <114073048+Andrew191103@users.noreply.github.com>
2023-01-24 15:55:06 +0700

    Update mylib.c

--- mylib.c
+++ mylib.c
@@ -1,4 +1,4 @@
-void states(int s0, int s1, int b, int* S0, int* S1, int* o0, int* 01) {
+void states(int s0, int s1, int b, int* S0, int* S1, int* o0, int* o1) {
     *S0 = (!s0) & b;
     *S1 = (!s0) &s1;
     *o0 =  (!s0) & b;
